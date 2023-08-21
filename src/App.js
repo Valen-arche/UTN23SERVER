1cee6fc242ad43beb0314fe7be9468a638c10826
Valen-arche <110450286+Valen-arche@users.noreply.github.com>
2023-08-20 23:12:46 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from "./pages/Home";
+import Contacto from './pages/Contacto';
+import Servicios from './pages/Servicios';
+import MapaAstral from './pages/MapaAstral';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Body from './components/Body';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className='App'>
+
+ 
+  <BrowserRouter> 
+   <Header/>
+  <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Contacto" element= {<Contacto/>} />
+        <Route path="/Servicios" element= {<Servicios/>} />
+        <Route path="/MapaAstral" element= {<MapaAstral/>} />
+        </Routes>
+        </BrowserRouter>
+
+         <Body/><Footer/>
+
+        </div>
+
+
   );
+ 
 }
 
 export default App;
